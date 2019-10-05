2608b0c833e3a5d9aaddd5838640351311153596
Nikhil1999 <nikhilkoshty@gmail.com>
2019-10-05 16:09:58 +0530

    Added Confirm Password

---
+++ Article Generator/Central Data/app.js
@@ -0,0 +1,69 @@
+//Express Server Config
+var express = require('express');
+var expressLayouts = require('express-ejs-layouts');
+var session = require('express-session');
+var flash = require('connect-flash');
+var passport = require('passport');
+
+var app = express();
+
+//Assets
+app.use(express.static("assets"));
+
+//Passport Config
+require('./config/passport')(passport);
+
+//MongoDB Config
+const mongoose = require('mongoose');
+
+//EJS
+app.use(expressLayouts);
+app.set('view engine', 'ejs');
+
+//Bodyparser
+app.use(express.urlencoded({extended: true}));
+
+//Session
+app.use(session({
+    secret: 'secret',
+    resave: false,
+    saveUninitialized: true,
+}));
+
+//Passport
+app.use(passport.initialize());
+app.use(passport.session());
+
+//Flash
+app.use(flash());
+
+//Global Variables
+app.use(function(request, response, next) {
+    response.locals.success_message = request.flash('success_message');
+    response.locals.error_message = request.flash('error_message');
+    response.locals.error = request.flash('error');
+    next();
+});
+
+//Routes
+app.use('/', require('./routes/routes'));
+app.use('/login', require('./routes/routes'));
+app.use('/register', require('./routes/routes'));
+
+//Express Server Connection
+app.listen(3000, function(){
+    console.log("Server is now running");
+    console.log("Listening to port no : 3000");
+
+    //MongoDB Connection
+    mongoose.connect('mongodb://localhost/baps', {useNewUrlParser: true});
+    var mongoDB = mongoose.connection;
+
+    mongoDB.on('error', function(){
+        console.log("MongoDB connection failed");
+    });
+
+    mongoDB.once('open', function(){
+        console.log("MongoDB connected successfully");
+    });
+});
---
+++ Article Generator/Central Data/models/User.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    reset_password_token: {
+        type: String,
+        default: null
+    },
+    reset_password_expires: {
+        type: Date,
+        default: null
+    },
+    firstname: {
+        type: String,
+        required: true
+    },
+    lastname: {
+        type: String,
+        default: ''
+    },
+    created_on: {
+        type: Date,
+        default: Date.now()
+    }
+});
+
+const User = mongoose.model('user', UserSchema);
+
+module.exports = User;
